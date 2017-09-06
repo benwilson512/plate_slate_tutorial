83f120aecf647d88965cd48b462d6e171465a30b
Ben Wilson <benwilson512@gmail.com>
2017-09-06 09:42:00 -0700

    add descriptions to graphql

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -6,8 +6,12 @@ defmodule PlateSlateWeb.Schema do
     field :health, :string
   end
 
+  @desc """
+  Tasty thing to eat!
+  """
   object :menu_item do
     field :id, :id
+    @desc "The name of the item"
     field :name, :string
     field :description, :string
   end
