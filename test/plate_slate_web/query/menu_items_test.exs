dd6553c31eacf4bb91df05061b9ea71fd89d3eb4
Ben Wilson <benwilson512@gmail.com>
2017-09-06 11:25:37 -0700

    post example

--- test/plate_slate_web/query/menu_items_test.exs
+++ test/plate_slate_web/query/menu_items_test.exs
@@ -17,4 +17,15 @@ defmodule PlateSlateWeb.Query.MenuItemsTest do
     assert %{"data" => %{"menuItems" => [item |_] }} = json_response(conn, 200)
     assert item == %{"name" => "Rueben"}
   end
+
+  test "list menu items without filter with POST" do
+    query = "{ menuItems { name } }"
+    conn =
+      build_conn()
+      |> Plug.Conn.put_req_header("content-type", "application/json")
+      |> post("/", %{"query" => query})
+
+    assert %{"data" => %{"menuItems" => [item |_] }} = json_response(conn, 200)
+    assert item == %{"name" => "Rueben"}
+  end
 end
